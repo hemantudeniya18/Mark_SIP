abb613234ab9fa30b0f43d443c3f483886c71807
Hemant_u <61248952+hemantudeniya18@users.noreply.github.com>
2022-07-20 20:32:02 +0530

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mark1', {
+const dotenv = require('dotenv')
+const uri = "mongodb+srv://hyunjin0327:Yg9sw6U6uL4CPo52@grm.qcgqg.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
     // useCreateIndex: true,
     // useFindAndModify: true,
     useNewUrlParser: true,
@@ -14,22 +17,3 @@ conn.on('disconnected',function(){
     console.log('database is disconnected successfully');
 })
 conn.on('error', console.error.bind(console, 'connection error:'));
-
-/* 
-var mongodb=require('mongodb');  
-var MongoClient=mongodb.MongoClient;  
-var url="mongodb://localhost:27017/mark2"  
-MongoClient.connect(url,function(error,databases){  
-    if(error){  
-        throw error;  
-  
-    }  
-   /*  var dbase=databases.db("mark2");  
-    dbase.createCollection("pract",function(error,response){  
-        if(error){  
-            throw error;  
-        }   */
-      
-    // console.log("collection is created.....")  
-    // databases.close();  
-    // });   */
